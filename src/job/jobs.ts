01944dde8117c4e93a326082e6c553bdd0bb92b1
Chua Chiah Soon <chiahsoon18@gmail.com>
2022-02-16 14:41:52 +0800

    feat: slight change to cronjob frequency

--- src/job/jobs.ts
+++ src/job/jobs.ts
@@ -8,10 +8,10 @@ export const scheduleJobs = () => {
     const timestampService = new OriginStamp(API_KEY, 2);
     const jobs = [
         new CronJob('0 0 0 * * *',
-            getTimestampAllFn(timestampService),
-            null, false),
-        new CronJob('0 0 0 * * *',
             getUpdateVerificationLinkFn(timestampService),
+            null, false),
+        new CronJob('0 0 10 * * *',
+            getTimestampAllFn(timestampService),
             null, false),
     ];
 
